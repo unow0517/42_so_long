7f6c1e37c1ece0a3f3ee391e04f26fa05c066f94
unow0517 <unow1992@gmail.com>
2024-03-25 12:36:09 +0100

    add ft_printf without ./git

---
+++ ft_printf/libft/ft_strlen.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yowoo <yowoo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 11:20:56 by yowoo             #+#    #+#             */
+/*   Updated: 2024/03/21 14:04:04 by yowoo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlen(const char *str)
+{
+	unsigned int	len;
+
+	len = 0;
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+
+// #include <stdio.h>
+// int main(){
+//     char str[] = "";
+//     unsigned int len = ft_strlen(str);
+//     printf("%u", len);
+// }
